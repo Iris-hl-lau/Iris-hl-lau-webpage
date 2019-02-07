b169f0e018fdc2dc34fb57a83497cc2639e79642
Iris-hl-lau <98.iris.lau@gmail.com>
2019-02-06 19:47:25 -0800

    lab4

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
-let http = require('http');
-http.createServer(function(request, response) {
-	response.writeHead(200, {'Content-type' : 'text/plain'});
-    // response.write("Response's coming from server ... \n");
-    let time = new Date(); 
+// let http = require('http');
+// http.createServer(function(request, response) {
+// 	response.writeHead(200, {'Content-type' : 'text/plain'});
+//     // response.write("Response's coming from server ... \n");
+//     let time = new Date(); 
     
-    let curTime = String(time);
-    response.write(curTime);
-	response.end();
-	}
-).listen(8000);
-console.log('listening ... ');
+//     let curTime = String(time);
+//     response.write(curTime);
+// 	response.end();
+// 	}
+// ).listen(8000);
+// console.log('listening ... ');
 
+let http = require('http');
+let now = new Date();
+
+let port=process.env.PORT || 8080
+const server = http.createServer((req, res) => {
+//res.statusCode = 200;
+res.writeHead(200,{'Content-type': 'text/plain'});
+//responce.write('Date: ' + now);
+res.end('Date: ' + now);
+});
+server.listen(port,() => {
+console.log('listening....');
+});
