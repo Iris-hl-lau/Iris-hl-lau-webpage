2ead92bdc71ac3ff967596d1c0d34e0ce9adb84b
Iris-hl-lau <98.iris.lau@gmail.com>
2019-02-07 09:26:00 -0800

    lab4

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ let date = new Date();
 let port = process.env.PORT || 8080
 http.createServer((request, response) => {
 response.writeHead(200,{'Content-type': 'text/plain'});
+date.setUTCHours(date.getHours() - 8);
+date.toLocaleTimeString('en-US');
 response.end(String(date));
 }).listen(port,() => {
 console.log('listening...');
