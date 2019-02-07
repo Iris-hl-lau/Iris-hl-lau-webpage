9065a3b5f1c3e894ad173d7b334450c34e812d5d
Iris-hl-lau <98.iris.lau@gmail.com>
2019-02-06 21:31:36 -0800

    lab4

--- app.js
+++ app.js
@@ -2,14 +2,14 @@
 
 let http = require('http');
 http.createServer(function(request, response) {
+    response.statusCode = 200;
 	response.writeHead(200, {'Content-type' : 'text/plain'});
     // response.write("Response's coming from server ... \n");
     let time = new Date(); 
-    
     let curTime = String(time);
 	response.end(curTime);
 	}
-).listen(8000);
+).listen(8080 || process.env.PORT);
 console.log('listening ... ');
 
 // let http = require('http');
