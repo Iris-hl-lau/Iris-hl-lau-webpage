ea52b852c370e0be41a31fb35ed9a9125e3971b2
Iris-hl-lau <98.iris.lau@gmail.com>
2019-02-06 22:00:48 -0800

    lab4

--- app.js
+++ app.js
@@ -1,23 +1,10 @@
 let http = require('http');
 let date = new Date();
 
-let port=process.env.PORT || 8080
-const server = http.createServer((request, response) => {
+let port = process.env.PORT || 8080
+http.createServer((request, response) => {
 response.writeHead(200,{'Content-type': 'text/plain'});
 response.end(String(date));
-});
-server.listen(port,() => {
+}).listen(port,() => {
 console.log('listening...');
 });
-
-// let http = require('http');
-// let date = new Date();
-
-// let port=process.env.PORT || 8080
-// const server = http.createServer((request, response) => {
-// response.writeHead(200,{'Content-type': 'text/plain'});
-// response.end(date);
-// });
-// server.listen(port,() => {
-// console.log('listening....');
-// });
