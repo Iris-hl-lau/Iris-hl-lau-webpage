f1fd2ce2a5e901c07e0eaeb8a2dd4a88723e19af
Iris-hl-lau <98.iris.lau@gmail.com>
2019-02-06 21:21:08 -0800

    lab4

--- app.js
+++ app.js
@@ -1,26 +1,27 @@
-// let http = require('http');
-// http.createServer(function(request, response) {
-// 	response.writeHead(200, {'Content-type' : 'text/plain'});
-//     // response.write("Response's coming from server ... \n");
-//     let time = new Date(); 
-    
-//     let curTime = String(time);
-//     response.write(curTime);
-// 	response.end();
-// 	}
-// ).listen(8000);
-// console.log('listening ... ');
+'use strict';
 
 let http = require('http');
-let now = new Date();
+http.createServer(function(request, response) {
+	response.writeHead(200, {'Content-type' : 'text/plain'});
+    // response.write("Response's coming from server ... \n");
+    let time = new Date(); 
+    
+    let curTime = String(time);
+	response.end(curTime);
+	}
+).listen(8000);
+console.log('listening ... ');
 
-let port=process.env.PORT || 8080
-const server = http.createServer((req, res) => {
-//res.statusCode = 200;
-res.writeHead(200,{'Content-type': 'text/plain'});
-//responce.write('Date: ' + now);
-res.end('Date: ' + now);
-});
-server.listen(port,() => {
-console.log('listening....');
-});
+// let http = require('http');
+// let date = new Date();
+
+// let port=process.env.PORT || 8080
+// const server = http.createServer((req, res) => {
+// //res.statusCode = 200;
+// res.writeHead(200,{'Content-type': 'text/plain'});
+// //response.write('Date: ' + now);
+// res.end(date);
+// });
+// server.listen(port,() => {
+// console.log('listening....');
+// });
