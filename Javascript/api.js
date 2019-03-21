296fbd9ff91453692b26475c1d0eb4b5e324f95e
Iris-hl-lau <98.iris.lau@gmail.com>
2019-03-20 17:50:35 -0700

    lab7

--- Javascript/api.js
+++ Javascript/api.js
@@ -10,7 +10,7 @@ gapi.client.load('youtube', 'v3', function() {
 });
 
 function request() {
-  let searchVid = document.querySelector('#searchVid');
+  let q = document.querySelector('#searchVid').val();
   let req = gapi.client.youtube.search.list({
     q: q,
     part: 'snippet',
@@ -23,23 +23,7 @@ function request() {
       vidName = item.snippet.title;
       $('#result').append('<pre>' + vidName + '</pre>');
     })
-  })
+  });
+  }
 }
 
-// /**
-//  * This function searches for videos related to the keyword 'dogs'. The video IDs and titles
-//  * of the search results are logged to Apps Script's log.
-//  *
-//  * Note that this sample limits the results to 25. To return more results, pass
-//  * additional parameters as documented here:
-//  *   https://developers.google.com/youtube/v3/docs/search/list
-//  */
-// function searchByKeyword() {
-//     var results = YouTube.Search.list('id,snippet', {q: searchVid.value, maxResults: 25});
-//     for(var i in results.items) {
-//       var item = results.items[i];
-//       Logger.log('[%s] Title: %s', item.id.videoId, item.snippet.title);
-//     }
-//   }
-
-  
